975753105bbd85771bb4442403b22e3afb40a6b4
Bhavneet Verma <bhavneetv295@gmail.com>
2025-12-25 13:14:35 +0530

    Learn Imports and user define Imports

---
+++ importWorks/main.py
@@ -0,0 +1,12 @@
+import math as m
+import userDefineImports as u
+print(dir(m))
+print(m.sqrt(4))
+print(m.floor(m.pi) )
+
+us = u.Userdefine()
+us.MySelf()
+print(us.age)
+u.greet()
+
+
---
+++ importWorks/userDefineImports.py
@@ -0,0 +1,10 @@
+class Userdefine:
+
+    age = 19
+    def MySelf(self):
+        print("My UserDefine")
+
+
+
+def greet():
+    print("Hello World this is greet method in userDefine Imports")
